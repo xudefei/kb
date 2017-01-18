b66eb5dc1e5c43ba867d160bc25c1b9b5fca4b24
xdf <suda_xudefei@126.com>
2017-01-18 15:35:50 +0800

    新增滑动删除list view

---
+++ app/src/main/java/com/xdf/kb/getnetaddr/GetWifiMacActivity.java
@@ -0,0 +1,10 @@
+package com.xdf.kb.getnetaddr;
+
+import com.xdf.kb.base.BaseActivity;
+
+/**
+ * Created by Administrator on 2017/1/13.
+ */
+
+public class GetWifiMacActivity extends BaseActivity {
+}
